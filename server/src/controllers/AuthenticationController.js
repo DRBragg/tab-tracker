ee40e2ca59be742039329e6b12f27868aed39f62
DRBragg <DRBragg@gmail.com>
2017-11-04 16:34:45 -0400

    added jwt to user model, encrypted user password, added Vuex store for holding user token and information, created conditional render for buttons on header

--- server/src/controllers/AuthenticationController.js
+++ server/src/controllers/AuthenticationController.js
@@ -1,10 +1,21 @@
 const {User} = require('../models')
+const jwt = require('jsonwebtoken')
+const config = require('../config/config')
+
+function jwtSignUser (user) {
+  const ONE_WEEK = 60 * 60 * 24 * 7
+  return jwt.sign(user, config.authentication.jwtSecret,{
+    expiresIn: ONE_WEEK })
+}
 
 module.exports = {
   async register (req, res) {
     try {
       const user = await User.create(req.body)
-      res.send(user.toJSON())
+      res.send({
+        user: user.toJSON(),
+        token: jwtSignUser(user.toJSON())
+      })
     } catch(err) {
       res.status(400).send({
         error: 'This email account is already in use'
@@ -25,7 +36,7 @@ module.exports = {
         })
       }
 
-      const isPasswordValid = password === user.password
+      const isPasswordValid = await user.comparePassword(password)
       if (!isPasswordValid) {
         return res.status(403).send({
           error: 'Incorrect log in information'
@@ -33,7 +44,8 @@ module.exports = {
       }
 
       res.send({
-        user: user.toJSON()
+        user: user.toJSON(),
+        token: jwtSignUser(user.toJSON())
       })
     } catch(err) {
       res.status(500).send({
