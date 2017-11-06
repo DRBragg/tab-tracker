94a7e506dbdd40d39231124406430585be13aeae
DRBragg <DRBragg@gmail.com>
2017-11-06 13:48:29 -0500

    added post function, created create song view, est connection

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,7 +3,7 @@
     <v-app>
       <page-header />
       <main>
-        <v-container>
+        <v-container fluid>
           <router-view/>
         </v-container>
       </main>
@@ -13,17 +13,26 @@
 
 <script>
 import PageHeader from './components/PageHeader'
-import HelloWorld from './components/HelloWorld'
 
 export default {
   name: 'app',
   components: {
-    PageHeader,
-    HelloWorld
+    PageHeader
   }
 }
 </script>
 
 <style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.danger-alert {
+  color: red;
+}
 
 </style>
---
+++ client/src/components/CreateSong.vue
@@ -0,0 +1,103 @@
+<template>
+  <v-layout>
+    <v-flex xs4>
+      <panel title="Add Song">
+        <v-text-field
+          name="title"
+          label="Title"
+          v-model="song.title"
+        ></v-text-field>
+
+        <v-text-field
+          name="artist"
+          label="Artist"
+          v-model="song.artist"
+        ></v-text-field>
+
+        <v-text-field
+          name="genre"
+          label="Genre"
+          v-model="song.genre"
+        ></v-text-field>
+
+        <v-text-field
+          name="album"
+          label="Album"
+          v-model="song.album"
+        ></v-text-field>
+
+        <v-text-field
+          name="albumImage"
+          label="Album Image URL"
+          v-model="song.albumImage"
+        ></v-text-field>
+
+        <v-text-field
+          name="youtubeId"
+          label="Youtube Id"
+          v-model="song.youtubeId"
+        ></v-text-field>
+      </panel>
+    </v-flex>
+    <v-flex xs8>
+      <panel title="Song Data" class="ml-4">
+        <v-text-field
+          name="tab"
+          label="Tab"
+          v-model="song.tab"
+          multi-line
+        ></v-text-field>
+
+        <v-text-field
+          name="lyrics"
+          label="Lyrics"
+          v-model="song.lyrics"
+          multi-line
+        ></v-text-field>
+      </panel>
+      <v-btn dark class="cyan ml-4" @click="createSong">Create Song</v-btn>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import Panel from '@/components/Panel'
+import SongsService from '@/services/SongsService'
+
+export default {
+  data () {
+    return {
+      song: {
+        title: null,
+        artist: null,
+        genre: null,
+        album: null,
+        albumImage: null,
+        youtubeId: null,
+        lyrics: null,
+        tab: null
+      }
+    }
+  },
+  methods: {
+    async createSong () {
+      try {
+        await SongsService.post(this.song)
+        this.$router.push({
+          name: 'songs'
+        })
+      } catch (err) {
+        console.log(err)
+      }
+    }
+  },
+  components: {
+    Panel
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
